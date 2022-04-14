9e13f7812a4caa113c50fbe2bad615d3921e044b
arabellaloveridge <97570279+arabellaloveridge@users.noreply.github.com>
2022-04-13 23:00:31 -0400

    Delete Godot-3D-Template-main directory

--- Godot-3D-Template-main/Player/RayCast.gd
+++
@@ -1,9 +0,0 @@
-extends RayCast
-
-func _physics_process(_delta):
-	if is_colliding():
-		var pos = get_collision_point()
-		$Reticule.global_transform.origin = pos
-	elif $Reticule.translation != Vector3(0,0,50):
-		$Reticule.translation = Vector3(0,0,50)
-		
